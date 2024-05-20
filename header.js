9c316043bdd5ce6e8ab818dc5fe8f87285f5d08c
BubbaboyX <45315242+BubbaboyX@users.noreply.github.com>
2024-05-20 12:06:43 -0400

    Add files via upload

---
+++ header.js
@@ -0,0 +1,23 @@
+function set() {
+    const Home=document.querySelector("#Home")
+    const About=document.querySelector(".header-text#About")
+    const Games=document.querySelector(".header-text#Games")
+    const Account=document.querySelector(".header-text#Account")
+
+    Home.tabIndex=0
+    About.tabIndex=0
+    Games.tabIndex=0
+    Account.tabIndex=0
+
+    Home.onclick=() => {window.location.href="/"}
+    About.onclick=() => {window.location.href="about.html/"}
+    Games.onclick=() => {window.location.href="games.html/"}
+    Account.onclick=() => {window.location.href="account.html/"}
+
+    Home.onkeydown=(key) => {if (key.key=="Tab") return;window.location.href="/"}
+    About.onkeydown=(key) => {if (key.key=="Tab") return;window.location.href="about.html/"}
+    Games.onkeydown=(key) => {if (key.key=="Tab") return;window.location.href="games.html/"}
+    Account.onkeydown=(key) => {if (key.key=="Tab") return;window.location.href="account.html/"}
+}
+
+window.onload=set
