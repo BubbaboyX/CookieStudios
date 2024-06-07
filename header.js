484a383b92e7498ecb64986c3ead396e72e7606b
BubbaboyX <45315242+BubbaboyX@users.noreply.github.com>
2024-06-07 14:34:20 -0400

    Add files via upload

--- header.js
+++ header.js
@@ -4,12 +4,13 @@ function set() {
     const Games=document.createElement("p");Games.innerHTML="Games";Games.className="header-text"
     const Account=document.createElement("p");Account.innerHTML="Account";Account.className="header-text"
     const EE=document.createElement("p");EE.innerHTML="Easter Egg";EE.className="header-text"
-    const Sep=document.createElement("p");Sep.innerHTML="";Sep.className="header-text"
+	const OST=document.createElement("p");OST.innerHTML="OST";OST.className="header-text"
 
     Home.tabIndex=0
     About.tabIndex=0
     Games.tabIndex=0
     Account.tabIndex=0
+	OST.tabIndex=0
     EE.style.opacity="0%"
 
     Home.onclick=() => {window.location.href="index"}
@@ -17,16 +18,19 @@ function set() {
     Games.onclick=() => {window.location.href="games"}
     Account.onclick=() => {window.location.href="account"}
     EE.onclick=() => {window.location.href="ee"}
+	OST.onclick=() => {window.location.href="ost"}
     
     Home.onkeydown=(key) => {if (key.key=="Tab") return;window.location.href="index.html"}
     About.onkeydown=(key) => {if (key.key=="Tab") return;window.location.href="about.html"}
     Games.onkeydown=(key) => {if (key.key=="Tab") return;window.location.href="games.html"}
     Account.onkeydown=(key) => {if (key.key=="Tab") return;window.location.href="account.html"}
+	OST.onkeydown=(key) => {if (key.key=="Tab") return;window.location.href="ost.html"}
 
     document.body.querySelector("div#header").appendChild(Home)
     document.body.querySelector("div#header").appendChild(About)
     document.body.querySelector("div#header").appendChild(Games)
     document.body.querySelector("div#header").appendChild(Account)
+	document.body.querySelector("div#header").appendChild(OST)
     document.body.querySelector("div#header").appendChild(EE)
 }
 
